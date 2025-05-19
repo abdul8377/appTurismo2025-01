26e8a60597231c7b3446eb8409dc81668b068cff
EliasDemo <franckalbertsoncoaquirajusto@gmail.com>
2025-05-18 19:06:14 -0500

    resolviendo errores

--- appTurismo202501/appTurismo202501/app/src/main/java/pe/edu/upeu/appturismo202501/utils/AuthUtils.kt
+++
@@ -1,9 +0,0 @@
-package pe.edu.upeu.appturismo202501.utils
-
-import pe.edu.upeu.sysventasjpc.utils.TokenUtils
-
-object AuthUtils {
-    fun isAuthenticated(): Boolean {
-        return TokenUtils.TOKEN_CONTENT.isNotEmpty()
-    }
-}
