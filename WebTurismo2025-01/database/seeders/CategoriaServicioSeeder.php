69141a7abbb45177759c26cfba41b0b51818e4ae
EliasDemo <franckalbertsoncoaquirajusto@gmail.com>
2025-05-20 07:37:03 -0500

    Poblar base de datos

--- WebTurismo2025-01/database/seeders/CategoriaServicioSeeder.php
+++ WebTurismo2025-01/database/seeders/CategoriaServicioSeeder.php
@@ -7,44 +7,42 @@ use Illuminate\Database\Seeder;
 
 class CategoriaServicioSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
-    public function run(): void
+    public function run()
     {
         CategoriaServicio::create([
-            'nombre' => 'Cultura',
-            'descripcion' => 'Experiencias culturales',
-            'imagen' => 'images/cultura.jpg',
-            'icono' => 'iconos/cultura.png'
+            'nombre' => 'Turismo Vivencial',
+            'descripcion' => 'Experiencias auténticas donde los visitantes conviven con familias locales y aprenden sobre sus tradiciones.',
+            'imagen' => 'images/turismo_vivencial.jpg',
+            'icono' => 'iconos/turismo_vivencial.png'
+
+            /* id 1 */
         ]);
 
         CategoriaServicio::create([
-            'nombre' => 'Gastronomía',
-            'descripcion' => 'Experiencias culinarias',
-            'imagen' => 'images/gastronomia.webp',
-            'icono' => 'iconos/gastronomia.png'
+            'nombre' => 'Gastronomía Andina',
+            'descripcion' => 'Ofrecemos platos tradicionales como la torreja de quinua, sopa de illaco y t’himpo de pejerrey.',
+            'imagen' => 'images/gastronomia_andina.jpg',
+            'icono' => 'iconos/gastronomia_andina.png'
+
+            /* id 2 */
         ]);
 
         CategoriaServicio::create([
-            'nombre' => 'Experiencias',
-            'descripcion' => 'Experiencias inolvidables',
-            'imagen' => 'images/experiencias.jpg',
-            'icono' => 'iconos/experiencias.png'
+            'nombre' => 'Ecoturismo y Senderismo',
+            'descripcion' => 'Rutas naturales en la península de Capachica, con miradores panorámicos y observación de flora y fauna.',
+            'imagen' => 'images/ecoturismo_senderismo.jpg',
+            'icono' => 'iconos/ecoturismo_senderismo.png'
+
+            /* id 3 */
         ]);
 
         CategoriaServicio::create([
-            'nombre' => 'Alojamiento',
-            'descripcion' => 'Alojamientos para todos los gustos',
-            'imagen' => 'images/alojamiento.jpeg',
-            'icono' => 'iconos/alojamiento.png'
-        ]);
+            'nombre' => 'Pesca Artesanal',
+            'descripcion' => 'Participa en la pesca tradicional en el Lago Titicaca, utilizando métodos ancestrales.',
+            'imagen' => 'images/pesca_artesanal.jpg',
+            'icono' => 'iconos/pesca_artesanal.png'
 
-        CategoriaServicio::create([
-            'nombre' => 'Guías',
-            'descripcion' => 'Los mejores guías turísticos',
-            'imagen' => 'images/guia.jpg',
-            'icono' => 'iconos/guia.png'
+            /* id 4 */
         ]);
     }
 }
