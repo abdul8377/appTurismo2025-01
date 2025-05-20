2eca2939075812350f2ed4fc8ea811462c981b19
abdul8377 <abdulalfonso77@gmail.com>
2025-05-20 07:58:55 -0500

    chore: remove Gradle caches and build outputs from tracking

--- appTurismo202501/app/build/generated/hilt/component_trees/debug/dagger/hilt/internal/processedrootsentinel/codegen/_pe_edu_upeu_appturismo202501_MyApplication.java
+++
@@ -1,9 +0,0 @@
-package dagger.hilt.internal.processedrootsentinel.codegen;
-
-import dagger.hilt.internal.processedrootsentinel.ProcessedRootSentinel;
-
-@ProcessedRootSentinel(
-    roots = "pe.edu.upeu.appturismo202501.MyApplication"
-)
-public final class _pe_edu_upeu_appturismo202501_MyApplication {
-}
